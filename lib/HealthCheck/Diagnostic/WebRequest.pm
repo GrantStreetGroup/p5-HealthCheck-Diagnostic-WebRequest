693911f3aa56ac17ed442bc240768360007ee100
Brandon Messineo <brandon.messineo@grantstreet.com>
2018-08-20 13:22:29 -0400

    Add basic boilerplate for repo
    This allows the repo to be placed on gsgpan. It also allows tests to be
    written.

---
+++ lib/HealthCheck/Diagnostic/WebRequest.pm
@@ -0,0 +1,10 @@
+package HealthCheck::Diagnostic::WebRequest;
+
+# ABSTRACT: Make HTTP/HTTPS requests to web servers to check the connectivity
+# VERSION
+
+use strict;
+use warnings;
+use parent 'HealthCheck::Diagnostic';
+
+1;
---
+++ t/HealthCheck-Diagnostic-WebRequest.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+
+use Test::More;
+
+pass "Tests loaded";
+
+done_testing;
